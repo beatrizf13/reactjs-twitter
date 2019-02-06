af4a6797354c313ef2f3d0bcc3503ac3178e317f
beatrizf13 <azz.felix7@gmail.com>
2019-02-06 15:41:48 -0300

    login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 
+import Logo from '../../assets/twitter.svg';
 import { Container } from './styles';
 
 export default class login extends Component {
+  state = {
+    username: '',
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { username } = this.state;
+
+    if (!username.length) return;
+
+    localStorage.setItem('@Twitter: username', username);
+
+    this.props.history.push('/timeline');
+  };
+
+  handleInputChange = (e) => {
+    this.setState({ username: e.target.value });
+  };
+
   render() {
     return (
       <Container>
-        <h1>Login</h1>
+        <img src={Logo} alt="Logo" />
+        <form onSubmit={this.handleSubmit}>
+          <input
+            value={this.state.username}
+            onChange={this.handleInputChange}
+            placeholder="Username"
+          />
+          <button type="submit">Login</button>
+        </form>
       </Container>
     );
   }
