6118825031c296932c45c8fdf8ad452a5cb7a6cb
beatrizf13 <azz.felix7@gmail.com>
2019-02-06 16:03:32 -0300

    tweet

--- src/pages/timeline/index.js
+++ src/pages/timeline/index.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
 
+import Logo from '../../assets/twitter.svg';
 import { Container } from './styles';
 
 export default class login extends Component {
+  state = {
+    newTweet: '',
+  };
+
+  handleNewTweet = (e) => {
+    if (e.keyCode !== 13) return;
+
+    const content = this.state.newTweet;
+    const auhtor = localStorage.getItem('@Twitter: username');
+
+    console.log(content, auhtor);
+  };
+
+  handleInputChange = (e) => {
+    this.setState({ newTweet: e.target.value });
+  };
+
   render() {
     return (
       <Container>
-        <h1>Timeline</h1>
+        <img height={24} src={Logo} alt="Logo" />
+        <form>
+          <textarea
+            value={this.state.newTweet}
+            onChange={this.handleInputChange}
+            onKeyDown={this.handleNewTweet}
+            placeholder="What's happening?"
+          />
+        </form>
       </Container>
     );
   }
