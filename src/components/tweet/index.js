d42d6ea6e42d8f27914a63d1f603be26fbb38454
beatrizf13 <azz.felix7@gmail.com>
2019-02-06 18:09:16 -0300

    likes

--- src/components/tweet/index.js
+++ src/components/tweet/index.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import { Container } from './styles';
 
 import like from '../../assets/like.svg';
+import api from '../../services/api';
 
 export default class Tweet extends Component {
+  handleLike = async () => {
+    const { _id } = this.props.tweet;
+
+    await api.post(`/likes/${_id}`);
+  };
+
   render() {
     const { tweet } = this.props;
 
@@ -12,7 +19,7 @@ export default class Tweet extends Component {
         <li>
           <strong>{tweet.author}</strong>
           <p>{tweet.content}</p>
-          <button>
+          <button type="button" onClick={this.handleLike}>
             <img src={like} alt="Like" />
             {tweet.likes}
           </button>
